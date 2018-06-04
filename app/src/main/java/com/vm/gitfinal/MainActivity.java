060b4c6e9be2d919b691e820cb14e8e01f0eb6f9
owenhou <owenhou@Objectivasoftware.com>
2018-06-04 09:56:06 +0800

    add commit

--- app/src/main/java/com/vm/gitfinal/MainActivity.java
+++ app/src/main/java/com/vm/gitfinal/MainActivity.java
@@ -11,4 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /**
+     * feature-1
+     * display HelloWorld
+     * @param view
+     */
+    public void feature1(View view) {
+        if (btn_feature_1.getText().toString().equals("功能1")) {
+            btn_feature_1.setText(R.string.feature_1_dis);
+        } else {
+            btn_feature_1.setText(R.string.feature_1);
+        }
+    }
 }
